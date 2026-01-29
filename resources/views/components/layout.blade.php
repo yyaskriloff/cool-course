9171eabe815f32c7ae3ed4f69334524b84340abc
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2026-01-29 11:46:18 +0200

    add dynamic title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="en" data-theme="lofi">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chirper - Home</title>
+    <title>{{ isset($title) ? $title . ' - Cool Courses' : 'Cool Courses' }}</title>
     <link rel="preconnect" href="<https://fonts.bunny.net>">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
@@ -16,7 +15,7 @@
 <body class="min-h-screen flex flex-col bg-base-200 font-sans">
     <nav class="navbar bg-base-100">
         <div class="navbar-start">
-            <a href="/" class="btn btn-ghost text-xl">🐦 Chirper</a>
+            <a href="/" class="btn btn-ghost text-xl">Cool Courses</a>
         </div>
         <div class="navbar-end gap-2">
             <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
+    <x-slot:title>
+        Home
+    </x-slot:title>
+
     <div class="max-w-2xl mx-auto">
         <div class="card bg-base-100 shadow mt-8">
             <div class="card-body">
