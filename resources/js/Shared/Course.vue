ab2b07299470adeab7a0cb3e3c0fc277b6bfa456
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2026-02-05 12:07:28 +0200

    fix link to course

--- resources/js/Shared/Course.vue
+++ resources/js/Shared/Course.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Link } from '@inertiajs/vue3'
+
 defineProps({
     course: Object
 })
@@ -6,14 +8,13 @@ defineProps({
 
 
 <template>
-
     <div class="card bg-base-100 shadow">
         <div class="card-body flex flex-row">   
             <div class="flex-1">
                 <h2 class="card-title">{{ course.title }}</h2>
                 <p class="text-base-content/60">{{ course.description }}</p>
             </div>
-            <a href="/courses/{{ course.id }}" class="btn btn-primary btn-sm">View</a>
+            <Link :href="`/courses/${course.id}`" class="btn btn-primary btn-sm">View</Link>
         </div>
     </div>
 </template>
