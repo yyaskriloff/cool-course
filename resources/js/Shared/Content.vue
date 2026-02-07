82c2c486166d80c392dff0224bc805ae78a2f10f
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2026-02-07 22:57:28 +0200

    allow watching content without accesses needed

--- resources/js/Shared/Content.vue
+++ resources/js/Shared/Content.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Link } from '@inertiajs/vue3'
+import { Link, Form } from '@inertiajs/vue3'
 import {defineProps} from 'vue'
 
 const props = defineProps({
@@ -19,17 +19,16 @@ console.log({content, can})
             <h2 class="text-2xl font-bold">{{ content.title }}</h2>
             <p class="text-base-content/60">{{ content.description }}</p>
         </div>
-        <div v-if="can.update" class="flex flex-row gap-2">
-            <div class="flex flex-row gap-2">
-                <form :action="`/courses/${content.course_id}/content/${content.id}`" method="POST">
-                    <!-- @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-error btn-sm">Delete</button> -->
-                </form>
+        <div  class="flex flex-row gap-2">
+            <template v-if="can.update">
+
+                <Form :action="`/courses/${content.course_id}/content/${content.id}`" method="DELETE">
+                    <button type="submit" class="btn btn-error btn-sm">Delete</button>
+                </Form>
                 <Link :href="`/courses/${content.course_id}/content/${content.id}/edit`"
-                    class="btn btn-primary btn-sm">Edit</Link>
+                class="btn btn-primary btn-sm">Edit</Link>
+            </template>
                 <Link :href="`/courses/${content.course_id}/content/${content.id}`" class="btn btn-primary btn-sm">Watch</Link>
-            </div>
         </div>
     </div>
 </div>
